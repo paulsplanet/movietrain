9637d03f2ddc2072a227278b0ce60cb5ab5fa729
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-06-15 17:12:37 -0700

    updating Header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Router from "./Router";
-import Header from "./Header";
+import GlobalStyles from "./GlobalStyles";
+
 
 class App extends Component {
   render () {
     return (
       <>
-        <Header />
         <Router />
+        <GlobalStyles />
       </>
     );  
   }
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,15 +3,18 @@ import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom"
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Header from "./Header"
 
 export default () => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
-        
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/search" component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 )
